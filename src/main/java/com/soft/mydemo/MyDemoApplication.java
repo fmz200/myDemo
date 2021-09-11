f44936eb16ee5660af1663b7ea83c6095d752797
feiming <zfm6633@gmail.com>
2021-09-11 17:22:19 +0800

    更新文件

--- src/main/java/com/soft/mydemo/MyDemoApplication.java
+++ src/main/java/com/soft/mydemo/MyDemoApplication.java
@@ -2,6 +2,8 @@ package com.soft.mydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author admin
@@ -11,6 +13,21 @@ public class MyDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyDemoApplication.class, args);
+    }
+
+    /**
+     * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+     */
+    public static class ServletInitializer extends SpringBootServletInitializer {
+        public ServletInitializer() {
+            System.out.println("初始化ServletInitializer");
+        }
+
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            // 注意这里要指向原先用main方法执行的Application启动类
+            return builder.sources(MyDemoApplication.class);
+        }
     }
 
 }
