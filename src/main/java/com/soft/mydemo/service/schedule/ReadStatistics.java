c91db215d5a2bf351bf164f7574b6ee0274573b6
feiming <zfm6633@gmail.com>
2022-06-12 17:09:15 +0800

    1、修复定时任务不跑的问题 2、新建一个笔记.md

--- src/main/java/com/soft/mydemo/service/schedule/ReadStatistics.java
+++ src/main/java/com/soft/mydemo/service/schedule/ReadStatistics.java
@@ -1,14 +1,18 @@
-package com.soft.mydemo.service;
+package com.soft.mydemo.service.schedule;
 
+import com.soft.mydemo.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
+ * 统计每天的阅读数据
  * Created by fmz200 on 2021/08/07
  */
 @Component
-public class DataStatisticsComponent {
+@EnableScheduling
+public class ReadStatistics {
     @Autowired
     ArticleService articleService;
 
